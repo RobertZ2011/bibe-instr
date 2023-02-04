a04e96a2694e0d8b86529cdc9839038f01666c23
Robert Zieba <robert@zieba.site>
2023-02-04 09:42:14 -0700

    Add state instruction format

---
+++ src/state.rs
@@ -0,0 +1,16 @@
+use num_derive::{ FromPrimitive, ToPrimitive };
+
+use crate::Register;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, FromPrimitive, ToPrimitive)]
+pub enum Operation {
+    Read,
+    Write,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Instruction {
+    op: Operation,
+    reg: Register,
+    imm: u32,
+}
